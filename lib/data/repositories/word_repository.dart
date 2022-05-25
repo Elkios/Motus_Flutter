ccf9eda1148e175b3bd9e53f918617e0b5a05d79
Mathys Pomier <pomier.mathys@outlook.fr>
2022-05-25 12:02:22 +0200

    add diatrics for remove accent and lowercase word

--- lib/data/repositories/word_repository.dart
+++ lib/data/repositories/word_repository.dart
@@ -1,3 +1,4 @@
+import 'package:diacritic/diacritic.dart';
 import 'package:flutter/services.dart';
 import 'package:motus_flutter/data/dataSources/local/word_hive.dart';
 import 'package:motus_flutter/data/entities/word/word.dart';
@@ -27,7 +28,7 @@ class WordRepository {
       String dico = await rootBundle.loadString('assets/files/dico.txt');
       List<String> filteredDico = dico.split('\n').where((word) => word.length >= 5 && word.length <= 9).toList();
       filteredDico.asMap().forEach((index, text) {
-        insertWord(Word(index, text));
+        insertWord(Word(index, removeDiacritics(text.toLowerCase())));
       });
     }
     return _wordHive!.getAllWords();
